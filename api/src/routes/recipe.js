ef7a3c4fbbcf7ba19e180645e2c54f2f6b8469e5
Santiago Miranda <100096337+Santiago-Miranda@users.noreply.github.com>
2022-08-19 13:55:38 -0300

    Update recipe.js

--- api/src/routes/recipe.js
+++ api/src/routes/recipe.js
@@ -7,9 +7,24 @@ const router = Router();
 
 
 router.post('/',async(req,res,next)=>{
-    const {name,image,tipoDePlato,resumenDePlato,nivelSalubre,pasos,diets} = req.body;
+    const {
+           name,
+           image,
+           tipoDePlato,
+           resumenDePlato,
+           nivelSalubre,
+           pasos,
+           diets
+          } = req.body;
     try {
-        await Recipe.create({name,image,tipoDePlato,resumenDePlato,nivelSalubre,pasos})
+        await Recipe.create({
+            name,
+            image,
+            tipoDePlato,
+            resumenDePlato,
+            nivelSalubre,
+            pasos
+        })
         if(diets && diets.length && typeof diets === 'object'){
             const newReci = await Recipe.findOne({where: {name}});
             diets.forEach(async d=>{
@@ -21,7 +36,15 @@ router.post('/',async(req,res,next)=>{
         next(error)
     }
 })
-
+router.delete('/:idReceta', (req, res, next) =>{
+    try{
+        const {idReceta} = req.params
+        Recipe.destroy({where: {id:idReceta}})
+        res.send('Deleted' + idReceta)
+    }catch(error){
+        next(error)
+    }
+});
 
 
 module.exports = router;
